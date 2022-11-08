25f8b718c591d8334a5330fdda6beb626cb30961
AlaaCool25 <97164012+AlaaCool25@users.noreply.github.com>
2022-11-08 20:42:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+noseX = 0;
+noseY = 0;
+
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550,500);
@@ -14,7 +21,11 @@ function setup() {
  }
 
 function draw() {
-    background('#E6E6FA');
+background('#E6E6FA');
+    document.getElementById("square_side").innerHTML = " Width and Height of Square is = " + difference + "px";
+    fill('#FAFA33');
+    stroke('#880808');
+    square(noseX, noseY, difference);
 }
 
 function gotPose(results) 
@@ -22,5 +33,15 @@ function gotPose(results)
     if (results.length > 0) 
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX  + "noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor( leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
+
     }
 }
